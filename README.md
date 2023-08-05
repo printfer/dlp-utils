5a35d34ca02f2d6ecfcb8d69d639919b19889b06
printfer <16911871+printfer@users.noreply.github.com>
2023-08-05 07:37:22 -0300

    init dlp-utils script

---
+++ README.md
@@ -0,0 +1,44 @@
+# dlp-utils
+
+This is a utility script for `yt-dlp`, a powerful command-line program used to download videos from various video platforms.
+
+## Usage
+
+To use `dlp-utils`, first download the script to your local system. Then change the permissions of the script to make it executable:
+
+```bash
+chmod +x dlp-utils
+```
+
+After that, simply run:
+
+```bash
+./dlp-utils [OPTIONS] <playlist URL>
+```
+
+If you want to run the script directly from an online source without manually downloading it, you can do so with `curl` or `wget`.
+
+Using curl:
+
+```bash
+curl -sSL https://raw.githubusercontent.com/printfer/dlp-utils/master/dlp-utils.sh | sh -s -- [OPTIONS] <playlist URL>
+```
+
+Using wget:
+
+```bash
+wget -qO- https://raw.githubusercontent.com/printfer/dlp-utils/master/dlp-utils.sh | sh -s -- [OPTIONS] <playlist URL>
+```
+
+## Notes
+
+* You must provide a valid playlist URL.
+* The script checks if `yt-dlp` is installed before running. If `yt-dlp` is not installed, it will print a message asking to install `yt-dlp`.
+
+## License
+
+[![](https://www.gnu.org/graphics/agplv3-with-text-162x68.png)](https://www.gnu.org/licenses/agpl-3.0.html)
+
+This project is licensed under the AGPLv3 License. See the [LICENSE](LICENSE) file for more information.
+
+Copyright © 2023 [Printfer](https://github.com/printfer)
---
+++ dlp-utils.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+# The program name and version
+PROG_NAME="dlp-utils"
+VERSION="1.0"
+
+# Function to show help message
+show_help() {
+    echo "Usage: $PROG_NAME [OPTIONS] <playlist URL>"
+    echo "Options:"
+    echo "  -h, --help         Display this help message"
+    echo "  -a, --audio-only   Download only audio from the videos"
+}
+
+# Function to check if yt-dlp is installed
+check_yt_dlp() {
+    if ! command -v yt-dlp > /dev/null; then
+        echo "$PROG_NAME: yt-dlp is required but it's not installed."
+        exit 1
+    fi
+}
+
+# Main function
+main() {
+    check_yt_dlp
+    # Default format
+    FORMAT=""
+
+    while :; do
+        case $1 in
+            -h|--help)
+                show_help
+                exit
+            ;;
+            -a|--audio-only)
+                FORMAT="-f bestaudio"
+            ;;
+            --)
+                shift
+                break
+            ;;
+            -*)
+                echo "$PROG_NAME: Invalid option: $1"
+                show_help
+                exit 1
+            ;;
+            *)
+                break
+        esac
+        shift
+    done
+
+    PLAYLIST_URL="$1"
+
+    if [ -z "$PLAYLIST_URL" ]; then
+        echo "$PROG_NAME: No playlist URL provided."
+        show_help
+        exit 1
+    fi
+
+    # Extract the playlist's name
+    PLAYLIST_NAME=$(yt-dlp --flat-playlist --playlist-items 1 --print "%(playlist_title)s" "$PLAYLIST_URL")
+
+    # Create a playlist file with the playlist's name
+    echo "#EXTM3U" > "$PLAYLIST_NAME.m3u"
+
+    # Download videos and write titles to the playlist file
+    yt-dlp $FORMAT --write-subs --exec "echo {} >> \"$PLAYLIST_NAME.m3u\"" "$PLAYLIST_URL"
+}
+
+# Call the main function with all command line arguments
+main "$@"
